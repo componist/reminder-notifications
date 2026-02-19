08593106eff3c6e2a0d809196c6afa45a8e9e403
Reinhold Jesse <reinhold.jesse@gmx.de>
2026-02-19 19:41:19 +0100

    update livewire v4

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('dashboard/reminder-notification')->name('package.reminder-notification.')->group(function () {
-    Route::get('/', \Componist\ReminderNotifications\Livewire\ReminderNotification\Index::class)->name('index');
-    Route::get('/create', \Componist\ReminderNotifications\Livewire\ReminderNotification\Create::class)->name('create');
-    Route::get('/{editElement}/edit', \Componist\ReminderNotifications\Livewire\ReminderNotification\Edit::class)->name('edit');
+    Route::livewire('/', \Componist\ReminderNotifications\Livewire\ReminderNotification\Index::class)->name('index');
+    Route::livewire('/create', \Componist\ReminderNotifications\Livewire\ReminderNotification\Create::class)->name('create');
+    Route::livewire('/{editElement}/edit', \Componist\ReminderNotifications\Livewire\ReminderNotification\Edit::class)->name('edit');
 });
